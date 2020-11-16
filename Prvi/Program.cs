79f9c8f3663541391f4b4eaf0af397bafba0ede2
mrvic <mrvic.predrag@gmail.com>
2020-11-16 20:36:24 +0100

    Prvi init

---
+++ Prvi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prvi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
