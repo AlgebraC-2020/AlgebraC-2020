81ea66377bfb6d14c8089e472f11dea30b556c94
mrvic <mrvic.predrag@gmail.com>
2020-11-16 21:09:00 +0100

    Drugi

---
+++ drugi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace drugi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
