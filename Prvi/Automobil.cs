71ab36a8d191f0db787263fc2366ba0a2a11059c
Mrvic <mrvic.predrag@gmail.com>
2020-11-24 20:29:47 +0100

    Napravili smo klasu Automobil

---
+++ Prvi/Automobil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prvi
+{
+    class Automobil
+    {
+        public string boja="Crvena";
+        string MarkaAutomobila;
+        double KS;
+        public override string ToString()
+        {
+            // return base.ToString();
+            return "Ja sam Automobil Audi A1, moja boja je "+this.boja;
+        }
+    }
+}
