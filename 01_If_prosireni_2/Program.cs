4ba76c608482941bf97169d0b71faff71baa3897
mrvic <mrvic.predrag@gmail.com>
2020-11-30 13:06:16 +0100

    If vjezbe

---
+++ 01_If_prosireni_2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _01_If_prosireni_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite broj:");
+            int broj = int.Parse(Console.ReadLine());
+
+            //Proširena if naredba
+            if (broj > 0)
+            {
+                Console.WriteLine("Broj je pozitivan.");
+            }
+            else if (broj < 0)
+            {
+                Console.WriteLine("Broj je negativan.");
+            }
+            else
+            {
+                Console.WriteLine("Nula !!!");
+            }
+
+            //Nested if
+            if (broj > 0)
+            {
+                Console.Write("Pozitivan");
+            }
+            else
+            {
+                if (broj < 0)
+                    Console.WriteLine("Negativan");
+                else
+                    Console.WriteLine("Nula (Zero)");
+            }
+
+            Console.ReadKey();
+
+            //5.2.2
+            //Kalkulator sa osnovnim operacijama (+,-)
+            //Traži unos 2 broja, a zatim unos operacije
+            //Rezultate ispisuje ukonzolni prozor.
+            //Ako je odabrana nepodržana operacija, ispisuje
+            //"Operacija nije podržana"
+
+
+
+        }
+    }
+}
