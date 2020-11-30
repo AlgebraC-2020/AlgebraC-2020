a070f395485fa5bc73e1cacc3063503188179278
mrvic <mrvic.predrag@gmail.com>
2020-11-30 20:16:40 +0100

    logicki operatori

---
+++ 05_logicki_OR/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05_logicki_OR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Direktor");
+
+            Console.WriteLine("unesi ime (Tomislav, Mislav ili Hrvoje)");
+            string uname = Console.ReadLine().ToLower();
+
+
+
+            if (uname == "tomislav" || uname == "mislav" || uname == "hrvoje")
+            {
+                Console.WriteLine("Dobar dan Direktore");
+            }
+            else
+            {
+                Console.WriteLine("Skartiraj se, nebitan si...");
+            }
+
+
+        }
+    }
+}
