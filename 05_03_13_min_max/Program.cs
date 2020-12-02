cd7311443d3cac92b54269a6ccbd4b2bf2be94ab
mrvic <mrvic.predrag@gmail.com>
2020-12-02 20:16:08 +0100

    min-max

---
+++ 05_03_13_min_max/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _05_03_13_min_max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi 10 brojeva");
+
+            int maxbroj=int.MinValue;  // trebamo postaviti na najmanju mogucu vrijednost uopce!
+            int minbroj=int.MaxValue;  // trebamo postaviti na najvecu mogucu vrijednost uopce!
+            int ulazniBroj;
+
+            int broj = 10;
+            while (broj>0)
+            {
+                ulazniBroj=int.Parse(Console.ReadLine());
+                if (ulazniBroj<minbroj)
+                {
+                    minbroj = ulazniBroj;
+                }
+                if (ulazniBroj > maxbroj)
+                {
+                    maxbroj = ulazniBroj;
+                }
+                broj--;
+            }
+            Console.WriteLine("Najmanji broj je {0}, a najveci je: {1}",minbroj,maxbroj);
+        }
+    }
+}
---
+++ 05_03_svi_savrse_1000/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _05_03_svi_savrse_1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("svi savrseni brojevi do 1000");
+
+
+            // SAVRSENI BROJ
+            //  https://www.enciklopedija.hr/natuknica.aspx?id=54771
+            //  6, 28, 496 i 8128. Grčki matematičar Arhita iz Tarenta
+
+            int brojacDjeljitelja = 0;
+            int savrsenBroj = 0;
+            for (int j = 0; j < 10000; j++)
+            {
+                brojacDjeljitelja = 0;
+                savrsenBroj = 0;
+                for (int i = j; i >= 1; i--)
+                {
+                    if (j % i == 0)
+                    {
+                        brojacDjeljitelja++;
+                        savrsenBroj += i;  // savrsen broj je onaj cija suma djeljitelja je jednaka njemu samom
+                                           //Console.Write(i + " ");
+                    }
+
+                }
+                //Console.WriteLine("\nUkupno ima {0} djeljitelja", brojacDjeljitelja);
+                if (j == savrsenBroj - j)
+                {
+                    Console.WriteLine("\nBroj {0} je savrsen jer je zbroj djeljitelja {1}", j, savrsenBroj - j);
+                }
+                else
+                {
+                  //  Console.WriteLine("\nBroj {0} nije savrsen jer je zbroj djeljitelja {1}", j, savrsenBroj - j);
+                }
+            }
+
+
+        }
+    }
+}
