dd36dac8ff79e4ac21daaa4c58eb868423cc81b1
mrvic <mrvic.predrag@gmail.com>
2020-12-02 18:59:34 +0100

    broj djeljitelja

--- 05_03_svi_djeljitelji/Program.cs
+++ 05_03_svi_djeljitelji/Program.cs
@@ -6,7 +6,28 @@ namespace _05_03_svi_djeljitelji
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Unesi broj, vracam djeljitelje:");
+            int nekibroj = 0;
+            try
+            {
+                nekibroj = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("nije broj" + e.Message);
+            }
+
+            int brojacDjeljitelja = 0;
+            for (int i = nekibroj; i >= 1; i--)
+            {
+                if (nekibroj % i == 0)
+                {
+                    brojacDjeljitelja++;
+                    Console.Write(i + " ");
+                }
+
+            }
+            Console.WriteLine("\nUkupno ima {0} djeljitelja", brojacDjeljitelja);
         }
     }
 }
