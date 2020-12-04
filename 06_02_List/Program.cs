bfdbbb5eaaa44b72d8b2926d096d2635f8077f5f
mrvic <mrvic.predrag@gmail.com>
2020-12-04 20:56:06 +0100

    primjeri liste

---
+++ 06_02_List/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06_02_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Genericke liste, unesi brojeve, unesi nulu za kraj");
+            List<int> listabrojeva = new List<int>();  // iskljucivo prima samo int !!!
+
+            List<int> parni = new List<int>();
+            List<int> neparni = new List<int>();
+
+
+            int broj = -1;
+            while (broj!=0) {
+                  broj=int.Parse(Console.ReadLine());
+                //broj = Console.ReadLine();
+                listabrojeva.Add(broj);
+            }
+
+            foreach (var item in listabrojeva)
+            {
+                Console.Write(item+" ");
+            }
+            foreach (var item in listabrojeva)
+            {
+                if (item % 2 == 0)  //parni
+                {
+                    parni.Add(item);
+                }
+                else
+                {
+                    neparni.Add(item);
+                }
+            }
+
+            Console.WriteLine("Parni:");
+            foreach (var item in parni)
+            {
+                Console.Write(item + " ");
+            }
+
+
+            Console.WriteLine("Neparni:");
+            foreach (var item in neparni)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("Sortirani neparni:");
+            neparni.Sort();
+            foreach (var item in neparni)
+            {
+                Console.Write(item + " ");
+            }
+
+        }
+    }
+}
