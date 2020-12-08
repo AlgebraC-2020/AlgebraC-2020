1c2d12a15484775105fe51d51682c51c84913ab8
mrvic <mrvic.predrag@gmail.com>
2020-12-08 20:09:19 +0100

    kvadrat potprogram

---
+++ 07_kvadrat/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _07_kvadrat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Kvadrat(5));
+            Console.WriteLine(Kvadrat(UcitajCijeliBroj()));
+            Console.WriteLine(Kvadrat("8"));
+
+            decimal kva = Kvadrat(5);
+            Console.WriteLine(kva);
+
+
+        }
+
+        private static int Kvadrat(string v)
+        {
+            return Kvadrat(int.Parse(v));
+        }
+
+        private static int UcitajCijeliBroj()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+
+        private static int Kvadrat(int v)
+        {
+            return v * v; 
+        }
+    }
+}
---
+++ 07_primjermetoda/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _07_primjermetoda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prvi pisemo glavni main program");
+
+
+        }
+    }
+}
