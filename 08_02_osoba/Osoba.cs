01cca7b2418928530e37be0ae84fe0119ddf63e8
mrvic <mrvic.predrag@gmail.com>
2020-12-14 20:12:15 +0100

    Primjer Objekti Tocka, Duljina Static

---
+++ 08_02_osoba/Osoba.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _08_02_osoba
+{
+    internal class Osoba
+    {
+        private int starost=5;
+        private string prezime;
+
+        public string Ime { get; internal set; }
+        internal string Prezime { get => prezime; set => prezime = value; }
+        internal int Starost { get { return starost; }
+
+            set {
+                if (value < 0)
+                {
+                    Exception ex = new Exception("Ne moze starost biti manja od nule");
+                }
+                else if (value > 120) {
+                    Exception ex = new Exception("Broj jedan jesi li to ti?");
+                }
+                else
+                {
+                    starost = value;
+                }
+                
+            }
+        }
+        internal string StarostPozdrav
+        {
+            get {
+                if (starost < 18)
+                {
+                    return "mladicu";
+                }
+                if (starost >= 18 && starost <= 60)
+                {
+                    return "gospodine";
+                }
+                else {
+                    return "postovani gospodine";
+                }
+            }
+        }
+
+    }
+}
---
+++ 08_02_osoba/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _08_02_osoba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Osoba os1 = new Osoba();
+            Console.WriteLine("Ime:");
+            os1.Ime = Console.ReadLine();
+            Console.WriteLine("Prezime:");
+            os1.Prezime = Console.ReadLine();
+
+            Console.WriteLine("Starost:");
+
+            try
+            {
+                os1.Starost = int.Parse(Console.ReadLine());
+                Console.WriteLine("Unos je ispravan!");
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Neispravamn unos "+e.Message);
+            }
+
+            Console.WriteLine("{3} Ime {0} prewzime {1} starost {2}", os1.Ime, os1.Prezime, os1.Starost, os1.StarostPozdrav);
+        }
+    }
+}
