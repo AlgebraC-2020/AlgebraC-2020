b56fd016c2055cc4829754c5c99c67e284858961
Marko <Marko>
2020-12-15 14:03:31 +0100

    2. DZ

---
+++ Zadaca_16_12/Kolac.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zadaca_16_12
+{
+    internal class Kolac
+    {
+        public string ImeKolaca { get; set; }
+        public bool Ispecen { get; set; }
+        public double UkupnaGramaza { get; set; }
+        public List<Sastojak> Sastojci = new List<Sastojak>();
+
+        public Kolac(string imeKolaca)
+        {
+            this.ImeKolaca = imeKolaca;
+            
+        }
+
+        internal void DodajSastojak(string imeSastojka, int gramaza)
+        {
+            if (Ispecen)
+            {
+                throw new Exception("Ne mozete dodavati sastojke, kolac je vec ispecen.");
+            }
+            else
+            {
+                Sastojci.Add(new Sastojak() { ImeSastojka = imeSastojka, Gramaza = gramaza });
+                UkupnaGramaza += gramaza;
+            }
+            
+        }
+
+        internal class Sastojak
+        {
+            public string ImeSastojka { get; set; }
+            public int Gramaza { get; set; }
+        }
+
+        public override string ToString()
+        {
+            if (Ispecen)
+            {
+                return "Kolac je pecen.";
+            }
+            else
+            {
+                return "Kolac nije pecen.";
+            }
+        }
+
+
+    }
+}
---
+++ Zadaca_16_12/Rerna.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zadaca_16_12
+{
+    static class Rerna
+    {
+        public static int BrojIspecenihKolaca;
+
+        public static void Ispeci(ref Kolac K1)
+        {
+            if (K1.ImeKolaca != null && K1.Sastojci.Count > 0)
+            {
+                BrojIspecenihKolaca++;
+                K1.Ispecen = true;
+            }
+            else
+            {
+                K1.Ispecen = false;
+            }
+           
+        }
+    }
+}
