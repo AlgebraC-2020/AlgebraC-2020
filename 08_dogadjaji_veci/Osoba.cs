e1ea3cb58638369b36d7187180f560e14d595706
mrvic <mrvic.predrag@gmail.com>
2020-12-18 20:53:14 +0100

    Siri primjer eventa sa CORE sintakcsom

---
+++ 08_dogadjaji_veci/Osoba.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _08_dogadjaji_veci
+{
+    internal class Osoba
+    {
+        private string ime;
+        private string prezime;
+
+        public string Ime { get => ime; set => ime = value; }
+        public string Prezime { get => prezime;
+
+            set {
+                // prezime nije Null i nije razmak te je novo razlicito od starog prezimena 
+                if (!string.IsNullOrWhiteSpace(prezime) && value != prezime)
+                {
+                    // ukoliko posti event pozovi ga
+                    if (PromijenjenoPrezime != null)
+                    {
+                        PromijenjenoPrezime(prezime, value);
+                    }
+                }
+                prezime = value;
+            } 
+        
+        }
+
+        public delegate void NaPromjenuPrezimenaDelegat(string staroPrezime, string novoPrezime);
+        public event NaPromjenuPrezimenaDelegat PromijenjenoPrezime;
+
+        public delegate void PozivPunogImenaDelegat();
+        public event PozivPunogImenaDelegat PozvanaMetodaPunoIme;
+
+
+        internal string PunoIme()
+        {
+            if (PozvanaMetodaPunoIme != null)
+            {
+                PozvanaMetodaPunoIme();
+            }
+            return "Moje ime je "+this.Ime + " " + this.Prezime;
+        }
+
+        
+    }
+}
---
+++ 08_dogadjaji_veci/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _08_dogadjaji_veci
+{
+    class Program
+    {
+
+        /// <summary>
+        /// Kada se promijeni prezime, treba 
+        /// ispisati: " Prezime {0} je promijenjeno u {1}"
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Osoba oso = new Osoba();
+            oso.PromijenjenoPrezime += PromjenaPrezimenaHandler;  //nakacio akciju na event
+            oso.PozvanaMetodaPunoIme += PozvanaMetodaPunoIme;
+            oso.Ime = "Mirko";
+            //1. postavljanje prezimena. Ne okida se event
+            oso.Prezime = "Spirko";
+            //2. Mijenjamo prezime => uvjet je da 
+            //prijašnje prezime nije bilo prazno i 
+            //da je novo prezime različito od prethodnog
+            oso.Prezime = "Fodor";
+
+            oso.PromijenjenoPrezime -= PromjenaPrezimenaHandler; //otkacio akciju s eventa
+            oso.Prezime = "Todor";
+
+
+            string punoIme = oso.PunoIme();
+            Console.WriteLine("Puno ime je:" + punoIme);
+            /*
+            //Preptplata na event Polaznika...
+            Polaznik polo = new Polaznik();
+            //5. Pretplata na event => Dajemo ime metodi koja će hendlati događaj
+            polo.DodjelaPOEvent += DodjelaProgramaObrazovanjaHandler;
+
+            polo.ProgramObrazovanja = "ASP.NET";
+*/
+            Console.ReadKey();
+
+        }
+        //6.Hendlanje događaja => Metoda koja ima isti potpis kao i delegat 
+        static void DodjelaProgramaObrazovanjaHandler(string dodijeljeniPO)
+        {
+            Console.WriteLine("Polaznika ste upisali u PO:{0}", dodijeljeniPO);
+        }
+
+        static void PromjenaPrezimenaHandler(string staro, string novo)
+        {
+            Console.WriteLine("Prezime {0} je promijenjeno u {1}",
+                staro, novo);
+        }
+
+
+        private static void PozvanaMetodaPunoIme()
+        {
+            Console.WriteLine("Netko je pozvao puno ime.");
+        }
+    }
+}
