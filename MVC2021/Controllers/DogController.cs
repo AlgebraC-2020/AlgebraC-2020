e4fa2b8819f1262064cd4fe2ad8e95b13deaa066
mrvic <mrvic.predrag@gmail.com>
2021-02-08 20:28:20 +0100

    Sljedeci puta kreiramo model

---
+++ MVC2021/Controllers/DogController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC2021.Controllers
+{
+    public class DogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // http://localhost:5000/Dog/Pozdrav?NekiBroj=5
+        public IActionResult Pozdrav(int NekiBroj = 1)
+        {
+            // neke varijable koje bacamo na view
+            ViewData["Poruka"] = "Uplati  mi na racun";
+            ViewData["NekiBroj"] = NekiBroj;
+
+
+            string model = "pozdrav svijete iz MVC-a!";
+            return View((object)model); //Views/Dog/Pozdrav.cshtml
+        }
+    }
+}
---
+++ MVC2021/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVC2021.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC2021.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        /**
+      * \Views\Home\Index.cshtml 
+      */
+        public IActionResult Index()  // defaultni action
+        {
+            return View();
+        }
+        /**
+         * \Views\Home\Privacy.cshtml 
+         */
+
+        public IActionResult Privacy()  
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
