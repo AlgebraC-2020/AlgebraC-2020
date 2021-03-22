22eb1f754e042dad0858374d9c64f9e2154a7b0f
mrvic <mrvic.predrag@gmail.com>
2021-03-22 18:55:20 +0100

    Proje kreiranja google id

--- OAuthGoogle/Controllers/HomeController.cs
+++ OAuthGoogle/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using OAuthGoogle.Models;
 using System;
@@ -23,6 +24,7 @@ namespace OAuthGoogle.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
