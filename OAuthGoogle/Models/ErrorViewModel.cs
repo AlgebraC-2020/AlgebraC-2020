7c7ce46b36410de84ae8680520d37ba10a404423
mrvic <mrvic.predrag@gmail.com>
2021-03-22 18:41:27 +0100

    Kreiran klasicni individual acc auth

---
+++ OAuthGoogle/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OAuthGoogle.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
